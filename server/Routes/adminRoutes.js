08593a30a713824dbbf4c0fb1ec41513066ebed1
rahulsaji69 <rahulsaji54@gmail.com>
2024-11-07 22:51:40 +0530

    Admin APi Issue solved

--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../Controllers/adminController');
+const adminController = require('../Controllers/adminController.js');
 
 router.get('/users', adminController.getUsers);
 
